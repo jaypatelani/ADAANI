b009245a2b8f6ac8343029cc0ac3065888ca6630
Jay Patel <jaypatelani@gmail.com>
2022-12-21 22:35:15 +0530

    Update Animation.adb

--- Reanimate/Animation.adb
+++ Reanimate/Animation.adb
@@ -1,70 +1,80 @@
 with Graphics.SvgTree;
+with Graphics.SvgTree.Printer;
+with Reanimate.Constants;
 with Reanimate.Ease;
 with Reanimate.Svg.Constructors;
+with Text.XML.Light.Output;
 
 package Reanimate.Animation is
 
    -- | Duration of an animation or effect. Usually measured in seconds.
-   type Duration is new Float;
-
+   type Duration is delta Double;
    -- | Time signal. Goes from 0 to 1, inclusive.
-   type Time is new Float;
+   type Time is Double;
 
    -- | SVG node.
-   type SVG is new Graphics.SvgTree.Tree;
+   type SVG is Tree;
 
    -- | Animations are SVGs over a finite time.
    type Animation is record
       Duration : Duration;
-      Frame : Time -> SVG;
+      Time_Function : Time -> SVG;
    end record;
 
-   -- | Construct an animation with a given duration. If the duration is not
-   --   positive raises 'Constraint_Error'.
-   function Mk_Animation (Duration : Duration; Frame : Time -> SVG) return Animation;
+   -- * Creating animations
+   function Mk_Animation (Duration : Duration; Time_Function : Time -> SVG) return Animation is
+begin
+   if Duration > 0.0 then
+      return Unsafe_Mk_Animation (Duration, Time_Function);
+   else
+      raise Constraint_Error with "Animation duration (" & Duration'Img & ") is not positive.";
+end Mk_Animation;
 
-   -- | Construct an animation with a given duration, without checking that the
-   --   duration is positive.
-   function Unsafe_Mk_Animation (Duration : Duration; Frame : Time -> SVG) return Animation;
+   function Unsafe_Mk_Animation (Duration : Duration; Time_Function : Time -> SVG) return Animation is
+begin
+   return Animation'(Duration, Time_Function);
+end Unsafe_Mk_Animation;
 
-   -- | Construct an animation with a duration of @1@.
-   function Animate (Frame : Time -> SVG) return Animation;
+   function Animate (Time_Function : Time -> SVG) return Animation is
+begin
+   return Animation'(1.0, Time_Function);
+end Animate;
 
-   -- | Create an animation with provided @duration@, which consists of stationary
-   --   frame displayed for its entire duration.
-   function Static_Frame (Duration : Duration; SVG : SVG) return Animation;
+   function StaticFrame (Duration : Duration; SVG : SVG) return Animation;
+   function Pause (Duration : Duration) return Animation;
 
-   -- | Query the duration of an animation.
+   -- * Querying animations
    function Duration (Animation : Animation) return Duration;
+   function Frame_At (Animation : Animation; Time : Time) return SVG;
 
-   -- | Play animations in sequence. The @lhs@ animation is removed after it has
-   --   completed. New animation duration is '@duration lhs + duration rhs@'.
-   --
-   --   Example:
-   --
-   --   @'Reanimate.Builtin.Documentation.Draw_Box' `'Seq_A'` 'Reanimate.Builtin.Documentation.Draw_Circle'@
-   function Seq_A (LHS, RHS : Animation) return Animation;
+   -- * Composing animations
+   function Seq_A (Animation_1, Animation_2 : Animation) return Animation;
+   function And_Then (Animation_1, Animation_2 : Animation) return Animation;
+   function Par_A (Animation_1, Animation_2 : Animation) return Animation;
+   function Par_Loop_A (Animation_1, Animation_2 : Animation) return Animation;
+   function Par_Drop_A (Animation_1, Animation_2 : Animation) return Animation;
 
-   -- | Play two animation concurrently. Shortest animation freezes on last frame.
-   --   New animation duration is '@max (duration lhs) (duration rhs)@'.
-   --
-   --   Example:
-   --
-   --   @'Reanimate.Builtin.Documentation.Draw_Box' `'Par_A'` 'Adjust_Duration' (*2) 'Reanimate.Builtin.Documentation.Draw_Circle'@
-   function Par_A (LHS, RHS : Animation) return Animation;
+   -- * Modifying animations
+   procedure Set_Duration (Animation : in out Animation; Duration : Duration);
+   procedure Adjust_Duration (Animation : in out Animation; Delta : Duration);
+   function Map_A (Animation : Animation; Map_Function : SVG -> SVG) return Animation;
+   function Take_A (Animation : Animation; Count : Positive) return Animation;
+   function Drop_A (Animation : Animation; Count : Positive) return Animation;
+   function Last_A (Animation : Animation) return Animation;
+   function Pause_At_End (Animation : Animation; Duration : Duration) return Animation;
+   function Pause_At_Beginning (Animation : Animation; Duration : Duration) return Animation;
+   function Pause_Around (Animation : Animation; Duration : Duration) return Animation;
+   function Repeat_A (Animation : Animation; Count : Positive) return Animation;
+   function Reverse_A (Animation : Animation) return Animation;
+   function Play_Then_Reverse_A (Animation : Animation) return Animation;
+   function Signal_A (Animation : Animation; Signal : Reanimate.Ease.Signal) return Animation;
+   function Freeze_At_Percentage (Animation : Animation; Percentage : Positive) return Animation;
+   function Add_Static (Animation : Animation; SVG : SVG) return Animation;
 
-   -- | Play two animation concurrently. Shortest animation repeats.
-   --   New animation duration is '@max (duration lhs) (duration rhs)@'.
-   --
-   --   Example:
-   --
-   --   @'Reanimate.Builtin.Documentation.Draw_Box' `'Par_Loop_A'` 'Adjust_Duration' (*2) 'Reanimate.Builtin.Documentation.Draw_Circle'@
-   function Par_Loop_A (LHS, RHS : Animation) return Animation;
+   -- * Misc
+   function Get_Animation_Frame (Animation : Animation; Time : Time) return SVG;
 
-   -- | Play two animation concurrently. LHS animation is removed after it has
-   --   completed.
-   --   New animation duration is '@duration rhs@'.
-   --
-   --   Example:
-   --
-   --   @'Reanimate.Built
+   type Sync is ...;
+
+   -- * Rendering
+   function Render_Tree (Animation : Animation
